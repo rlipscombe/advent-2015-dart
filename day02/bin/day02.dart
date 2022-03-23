94a1ab86ffb4949863051700c10873576f1bbab4
Roger Lipscombe <roger@differentpla.net>
2022-03-23 13:52:43 +0000

    day 2: Complete part 2

--- day02/bin/day02.dart
+++ day02/bin/day02.dart
@@ -6,7 +6,11 @@ void main(List<String> arguments) {
   // Each line is WxDxH.
   var lines = File("input.txt").readAsLinesSync();
   var presents = lines.map((line) => line.split("x").map(int.parse));
-  int result = presents.fold(
+  int part1 = presents.fold(
       0, (total, dimensions) => total + paperFor(dimensions.toList()));
-  print(result);
+  print(part1);
+
+  int part2 = presents.fold(
+      0, (total, dimensions) => total + ribbonFor(dimensions.toList()));
+  print(part2);
 }
