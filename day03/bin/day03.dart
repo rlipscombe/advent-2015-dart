754897aa57f98586b2a2e0eccbcd4bf6723d002c
Roger Lipscombe <roger@differentpla.net>
2022-03-23 15:16:37 +0000

    day 3: Complete part 1

--- day03/bin/day03.dart
+++ day03/bin/day03.dart
@@ -1,3 +1,8 @@
+import 'dart:io';
+
+import 'package:day03/day03.dart';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  String input = File('input.txt').readAsStringSync();
+  print(part1(input));
 }
--- day03/lib/day03.dart
+++ day03/lib/day03.dart
@@ -1,3 +1,30 @@
-int part1(input) {
-  return 0;
+import 'dart:collection';
+import 'dart:math';
+
+int part1(String input) {
+  var pos = Point(0, 0);
+
+  Set visited = HashSet();
+  visited.add(pos);
+
+  for (var i = 0; i < input.length; ++i) {
+    switch (input[i]) {
+      case '>':
+        pos += Point(1, 0);
+        break;
+      case '<':
+        pos += Point(-1, 0);
+        break;
+      case '^':
+        pos += Point(0, -1);
+        break;
+      case 'v':
+        pos += Point(0, 1);
+        break;
+    }
+
+    visited.add(pos);
+  }
+
+  return visited.length;
 }
