1743e5405648152bd3fa69e78c263fb4b107a727
Roger Lipscombe <roger@differentpla.net>
2022-03-23 12:28:54 +0000

    day 1: Complete part 2

--- day01/bin/day01.dart
+++ day01/bin/day01.dart
@@ -5,4 +5,5 @@ import 'package:day01/day01.dart';
 Future<void> main(List<String> arguments) async {
   var input = await File("input.txt").readAsString();
   print(part1(input));
+  print(part2(input));
 }
