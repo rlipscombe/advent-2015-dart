5240c22e64f7a6cb1481950b867ba9fe9ab3e420
Roger Lipscombe <roger@differentpla.net>
2022-03-23 12:22:56 +0000

    day 1: Complete part 1

--- day01/bin/day01.dart
+++ day01/bin/day01.dart
@@ -1,3 +1,8 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'dart:io';
+
+import 'package:day01/day01.dart';
+
+Future<void> main(List<String> arguments) async {
+  var input = await File("input.txt").readAsString();
+  print(day1(input));
 }
