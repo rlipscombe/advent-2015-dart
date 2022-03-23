8a3b910b18f9fba769a3ba607fd3f8225a39172a
Roger Lipscombe <roger@differentpla.net>
2022-03-23 13:47:52 +0000

    day 2: Use int.parse() directly

--- day02/bin/day02.dart
+++ day02/bin/day02.dart
@@ -5,7 +5,7 @@ import 'package:day02/day02.dart';
 void main(List<String> arguments) {
   // Each line is WxDxH.
   var lines = File("input.txt").readAsLinesSync();
-  var presents = lines.map((line) => line.split("x").map((e) => int.parse(e)));
+  var presents = lines.map((line) => line.split("x").map(int.parse));
   int result = presents.fold(
       0, (total, dimensions) => total + paperFor(dimensions.toList()));
   print(result);
