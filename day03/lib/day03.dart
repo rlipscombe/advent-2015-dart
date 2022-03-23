9b3e52330c5438618d8e3c59ce6fd89939b496fd
Roger Lipscombe <roger@differentpla.net>
2022-03-23 15:26:16 +0000

    day 3: Implement part 2

--- day03/lib/day03.dart
+++ day03/lib/day03.dart
@@ -1,7 +1,7 @@
 import 'dart:collection';
 import 'dart:math';
 
-int part1(String input) {
+Set followInstructions(String input) {
   var pos = Point(0, 0);
 
   Set visited = HashSet();
@@ -26,5 +26,25 @@ int part1(String input) {
     visited.add(pos);
   }
 
+  return visited;
+}
+
+int part1(String input) {
+  return followInstructions(input).length;
+}
+
+int part2(String input) {
+  String odds = '';
+  String evens = '';
+
+  for (var i = 0; i < input.length; ++i) {
+    if (i % 2 == 0) {
+      evens += input[i];
+    } else {
+      odds += input[i];
+    }
+  }
+
+  var visited = followInstructions(odds).union(followInstructions(evens));
   return visited.length;
 }
--- day03/test/day03_test.dart
+++ day03/test/day03_test.dart
@@ -2,13 +2,27 @@ import 'package:test/test.dart';
 import 'package:day03/day03.dart';
 
 void main() {
-  void assertPart1(input, expected) {
-    test('', () {
-      expect(part1(input), equals(expected));
-    });
-  }
+  group('part 1', () {
+    void assertPart1(input, expected) {
+      test('', () {
+        expect(part1(input), equals(expected));
+      });
+    }
 
-  assertPart1('>', 2);
-  assertPart1('^>v<', 4);
-  assertPart1('^v^v^v^v^v^v', 2);
+    assertPart1('>', 2);
+    assertPart1('^>v<', 4);
+    assertPart1('^v^v^v^v^v', 2);
+  });
+
+  group('part 2', () {
+    void assertPart2(input, expected) {
+      test('', () {
+        expect(part2(input), equals(expected));
+      });
+    }
+
+    assertPart2('^v', 3);
+    assertPart2('^>v<', 3);
+    assertPart2('^v^v^v^v^v', 11);
+  });
 }
