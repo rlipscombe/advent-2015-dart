628221bd9f7fb4367617f650882633cb76d30f85
Roger Lipscombe <roger@differentpla.net>
2022-03-24 09:13:30 +0000

    day 5: Implement 'count' extension method

--- day05/bin/day05.dart
+++ day05/bin/day05.dart
@@ -2,8 +2,14 @@ import 'dart:io';
 
 import 'package:day05/day05.dart';
 
+extension Functional<E> on Iterable<E> {
+  int count(bool Function(E elt) predicate) {
+    return fold(0, (acc, elt) => acc + (predicate(elt) ? 1 : 0));
+  }
+}
+
 void main(List<String> arguments) {
   var lines = File('input.txt').readAsLinesSync();
-  int count = lines.fold(0, (acc, s) => acc += isNiceString(s) ? 1 : 0);
+  int count = lines.count(isNiceString);
   print("part 1: $count");
 }
